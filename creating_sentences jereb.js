3b1bda6f5123cfad70c288d87bbb03d1e9fa9723
NathanJereb <95554800+NathanJereb@users.noreply.github.com>
2021-12-23 20:44:49 -0600

    Add files via upload

---
+++ creating_sentences jereb.js
@@ -0,0 +1,25 @@
+// TODO: Create an array variable called "words" with the following elements: 'This', 'is', 'JavaScript', 'Bootcamp!'
+// YOUR CODE GOES HERE
+const words = ['this', 'is', 'JavaScript', 'Bootcamp!']
+
+// TODO: Create an empty string variable called "sentence".
+// YOUR CODE GOES HERE
+sentence = []
+function createSentence(sentence){
+    for (let i = 0; i > 4; i++) {
+        let sentence = words[0];
+        console.log(sentence);
+        sentence = words + 1;
+      }
+      return sentence
+}
+// TODO: 
+// 1. Create a function called "createSentence" that takes an array as an argument.
+// 2. In the function use a for loop to iterate through each word element of the array.
+// 3. Add each word to the "sentence" variable.
+// 4. Return the "sentence".
+// YOUR CODE GOES HERE
+
+
+// TODO: Call the function "createSentence" using the console.log method.
+// YOUR CODE GOES HERE
