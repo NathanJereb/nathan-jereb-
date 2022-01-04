3065e7cd7237f1b812129f36c8390d77301a429b
NathanJereb <95554800+NathanJereb@users.noreply.github.com>
2022-01-03 19:40:10 -0600

    Add files via upload

---
+++ story_time.js
@@ -0,0 +1,29 @@
+// TODO: Create 4-String variables to introduce develop your story.
+let greetings = "hello";
+let name = 'Nathan';
+let object = "on the computer";
+let sit = "on the coutch";
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+let age = "15";
+let website = "youtube yideos";
+let date= "10/28/2021";
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+let rant = ['thats it','were you expecting a story?','ask somone else cause im bad at litercy','let me watch my cat videos in pease'];
+
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+let x = true;
+
+
+// TODO: Print your story to the console.
+console.log(greetings + ", my name is " + name + " and im " + age);
+console.log("its " + date);
+console.log("Im " + sit + " " + website + " " + object);
+console.log(rant[0])
+console.log(rant[1]);
+console.log(rant[2]);
+console.log(rant[3]);
+console.log(rant[4]);
+console.log("could have this been longer? " + x + ", but I do not care");
+console.log("end");
+
